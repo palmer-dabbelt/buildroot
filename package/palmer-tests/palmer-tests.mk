3629ac9d55c20b505de8e703a2bd013f28bc85ad
Palmer Dabbelt <palmer@dabbelt.com>
2025-06-18 12:52:50 -0700

    Bump palmer-tests
    Signed-off-by: Palmer Dabbelt <palmer@dabbelt.com>

--- package/palmer-tests/palmer-tests.mk
+++ package/palmer-tests/palmer-tests.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PALMER_TESTS_VERSION = b2d1e4ab6e3c0205f7da9dfc157e8a07157a0011
+PALMER_TESTS_VERSION = bc5db6357bb25b553e00f1781cdad1e7b73095cd
 PALMER_TESTS_LICENSE = BSD-3-Clause
 PALMER_TESTS_LICENSE_FILES = LICENSE
 PALMER_TESTS_SITE = $(call github,palmer-dabbelt,palmer-tests,$(PALMER_TESTS_VERSION))
